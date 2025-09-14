ae795c90b68c6c4839e4da883bce721003c12a67
armaan535kh <kh.armaan535@gmail.com>
2025-09-14 11:09:13 +0530

    Initial Commit

---
+++ LiveCurrency/LiveCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCurrencyApp.swift
+//  LiveCurrency
+//
+//  Created by Armaan Khan  on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveCurrencyTests/LiveCurrencyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCurrencyTests.swift
+//  LiveCurrencyTests
+//
+//  Created by Armaan Khan  on 14/09/25.
+//
+
+import Testing
+@testable import LiveCurrency
+
+struct LiveCurrencyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
