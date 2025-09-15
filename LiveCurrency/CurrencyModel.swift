256c93e0aef93479bb4c51d157a76f6ac629d962
armaan535kh <kh.armaan535@gmail.com>
2025-09-15 14:02:56 +0530

    [Update] Created model for storing currencies data

---
+++ LiveCurrency/CurrencyModel.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyModel.swift
+//  LiveCurrency
+//
+//  Created by Armaan Khan  on 15/09/25.
+//
+
+import Foundation
+
+
+struct Currency: Identifiable{
+    let id = UUID()
+    let name: String
+    let symbol: String
+    let conversionRate: Double
+    
+    
+}
+
+let currencies = [
+    Currency(name: "US Dollar", symbol: "USD", conversionRate: 1.0),
+    Currency(name: "Euro", symbol: "EUR", conversionRate: 0.89),
+    Currency(name: "Japanese Yen", symbol: "JPY", conversionRate: 130.0),
+    Currency(name: "Indian Rupee", symbol: "INR", conversionRate: 74.0),
+    
+]
---
+++ LiveCurrency/LiveCurrencyView.swift
@@ -0,0 +1,18 @@
+//
+//  LiveCurrencyView.swift
+//  LiveCurrency
+//
+//  Created by Armaan Khan  on 15/09/25.
+//
+
+import SwiftUI
+
+struct LiveCurrencyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiveCurrencyView()
+}
